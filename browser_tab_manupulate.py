8bbf271e01ddf6254699848936c0ea7140c9a8fe
takuwz <taku3wzr@gmail.com>
2018-09-23 14:01:12 +0900

    update to windows10
    change to check photo

--- browser_tab_manupulate.py
+++ browser_tab_manupulate.py
@@ -18,10 +18,14 @@ def next_tab():
 
 sleep(3)
 
-for i in range(0, 10):
+for i in range(0, 5):
     previous_tab()
-    rightClick(Pattern("1536713727981.png").targetOffset(14,-19))
-    click("1536713781636.png")
+    sleep(1)
+    #rightClick(Pattern("1536713727981.png").targetOffset(14,-19))
+    rightClick(Pattern("1537606554951.png").targetOffset(-6,-67))
+    sleep(1)
+    #click("1536713781636.png")
+    click("1537606634066.png")
     sleep(1)
     keyDown(Key.CTRL)
     type("w")
@@ -31,7 +35,8 @@ for i in range(0, 10):
     keyDown(Key.CTRL)
     type("v")
     keyUp(Key.CTRL)
-    # sleep(1)
+    sleep(1)
     type(Key.ENTER)
+    sleep(1) 
     type(Key.ENTER)
     sleep(1)
